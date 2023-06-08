0f49029d6461893d4be479ae145496523b0e91ce
soukaini adil <adilelwerdi81@gmail.com>
2023-06-08 16:28:05 -0100

    redux-toolkit-app-learning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./features/cart/CartContainer";
+import { useEffect } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [dispatch]);
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
+
+  const { isOpen } = useSelector((state) => state.modal);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 export default App;
